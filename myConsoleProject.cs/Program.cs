047f7e6a53c5fbd192b6ca511d75054ca2163a7c
Bhanuu098 <137154090+Bhanuu098@users.noreply.github.com>
2024-03-10 09:10:19 +0000

    Add files via upload
    use of \n and \"

---
+++ myConsoleProject.cs/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class SampleApplication
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems \nRemember, we’re \"accounting \" on you!");
+        
+        String name = Console.ReadLine();
+        
+    }
+}
