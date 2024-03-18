f9c527401ba979a552f9805f85fe5e30f721f2dd
Bhanuu098 <137154090+Bhanuu098@users.noreply.github.com>
2024-03-18 10:15:14 +0000

    Add files via upload
    Module 4 assignment

---
+++ Module4/Module4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4
+{
+    internal class Program //class
+    {
+        static int myMethod(int x, int y) // 2 integers
+        {
+            return 2* x;
+        }
+        static void Main(string[] args)
+        {
+            int y = myMethod(2, 4); //return the second integer on screen
+            Console.WriteLine(y); 
+            Console.ReadLine();
+        }
+    }
+}
