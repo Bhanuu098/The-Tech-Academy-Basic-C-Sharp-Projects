e77a3cdb187539dd3ef9259d2ac1fc223c69fa69
Bhanuu098 <137154090+Bhanuu098@users.noreply.github.com>
2024-03-21 16:56:53 +0000

    Add files via upload
    Module 4 Assignment

---
+++ Module4/Module4/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4
+{
+    class Class1
+    {
+        public int x;
+        public int y;
+
+        public Class1(int Ax, int Ay)
+        {
+            x = Ax;
+            y = Ay;
+        }
+        public int myMethod() {
+            return y = x * 2; //will return second integer y after operation on x
+        }
+    }
+}
---
+++ Module4/Module4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 first = new Class1(5, 10);
+            Console.WriteLine(first.myMethod()); //Calling Method
+            Console.ReadLine();
+        }
+    }
+}
