44a0cbd3b5633659685983f2b34b52483962b67b
Bhanuu098 <137154090+Bhanuu098@users.noreply.github.com>
2024-03-22 15:48:55 +0000

    Add files via upload
    Module 5 Assignment

---
+++ Module5/Module5/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5
+{
+    interface IQuittable
+    {
+        void Quit();
+    }
+    class Employee : IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("It is Quittable");
+        }
+        
+        
+    }
+
+    
+}
---
+++ Module5/Module5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee1 = new Employee();
+            Employee1.Quit();
+            IQuittable Employee2 = new Employee();
+            Employee2.Quit();
+            Console.ReadLine();
+        }
+    }
+}
