c251d98449a8e7bcbc2166ad5abab8a2c167565c
Bhanuu098 <137154090+Bhanuu098@users.noreply.github.com>
2024-03-24 08:43:22 +0000

    Add files via upload
    Operators overloading Assignment

---
+++ Module6/Module6/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module6
+{
+    public class Employee
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public Employee(int aId, string aFirstName, string aLastName)
+        {
+            Id = aId;
+            FirstName = aFirstName;
+            LastName = aLastName;
+        }
+     
+
+        public static bool operator == (Employee Employee1, Employee Employee2)
+        {
+            if (Employee1.Id.Equals(Employee2.Id)) {
+                return true;
+            }
+            return false;
+        }
+        public static bool operator != (Employee Employee1, Employee Employee2)
+        {
+            if (!Employee1.Id.Equals(Employee2.Id))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Module6/Module6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Employee1 = new Employee (3, "John", "Lewis");
+            Employee Employee2 = new Employee (6, "Riya", "Bansal");
+            if (Employee1 == Employee2)
+            {
+                Console.WriteLine("Employee 1 is equal to Employee 2");
+            }
+            else
+            {
+                Console.WriteLine("Employee 1 is not equal to Employee 2");
+            }
+            Console.ReadLine();
+        }
+    }
+}
